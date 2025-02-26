b321998ecc0f32bc005e3808025766df77c29614
basheer mohammad <basheer.m.mohammad@oracle.com>
2025-02-26 21:40:17 +0530

    02 26

--- src/pages/pageobjects/basepage.ts
+++ src/pages/pageobjects/basepage.ts
@@ -8,4 +8,32 @@ export default class BasePage {
         this.page = page;
     }
 
+    public getDataObjectFromCsv(data:Object[], fileName:string, index:number):Object{
+        if(!data.hasOwnProperty(fileName)){
+            throw new Error(`${fileName} does not exists in given test data object, pls check file exists with given name`);
+        }
+        if(index > data[fileName].length){
+            throw new Error(`test data of ${fileName} has max size of ${data[fileName].length} but looking for ${index}`);
+        }
+        let parsedData={}
+        for (const obj of Object.entries(data[fileName][index])) {
+            parsedData[obj[0]]=obj[1];
+        }
+        return parsedData;
+    }
+
+    public getDataMapFromCsv(data:Object[], fileName:string, index:number):Map<string,any>{
+        if(!data.hasOwnProperty(fileName)){
+            throw new Error(`${fileName} does not exists in given test data object, pls check file exists with given name`);
+        }
+        if(index > data[fileName].length){
+            throw new Error(`test data of ${fileName} has max size of ${data[fileName].length} but looking for ${index}`);
+        }
+        let parsedData=new Map<string,any>();
+        for (const obj of Object.entries(data[fileName][index])) {            
+            parsedData.set(obj[0], obj[1]);
+        }
+        return parsedData;
+    }
+
 }
--- src/tests/indicators/indicator.spec.ts
+++ src/tests/indicators/indicator.spec.ts
@@ -3,10 +3,13 @@ import { createJsonObjectFromFolder } from "../../utils/testdataloader";
 import { test, expect } from "../baseTest";
 
 
-test('add address test', { tag: ['@sanity'] }, async ({ loginPageObject, addressPageObject, testData }) => {
-        await addressPageObject.addTheAddress(testData["input2"]);
+test('add address test', { tag: ['@sanity123'] }, async ({ loginPageObject, addressPageObject, testCsvData }) => {
+        let dataObj1 = addressPageObject.getDataObjectFromCsv(testCsvData, "customers2", 1);
+        let dataObj2 = addressPageObject.getDataMapFromCsv(testCsvData, "customers2", 1);
+        console.log('data parsed');        
+        let dataObj3 = addressPageObject.getDataObjectFromCsv(testCsvData, "customers223", 1);
+        //await addressPageObject.addTheAddress(dataObj1);
 });
-
 
 test.describe('this is @sanityparam', async () => {
         let tJsonDataAll = await createJsonObjectFromFolder('src/data/json');
